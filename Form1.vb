ae3f4c8f665ea6d2eb446e322d596ba7744302b0
Administrator <Administrator@PSPKM2826-2254>
2022-03-28 11:01:03 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim score As Integer
+        Dim Result As String
+
+        score = InputBox("Enter score")
+        If score >= 0 And score <= 49 Then
+            Result = "Failed"
+        ElseIf score <= 74 And score >= 50 Then
+            Result = "Pass"
+        ElseIf score <= 89 And score >= 75 Then
+            Result = "Very Good"
+        ElseIf score >= 90 And score <= 100 Then
+            Result = "Excellent"
+        Else
+            Result = "Please input your score between 0 - 100"
+        End If
+
+        Label1.Text = Result
+    End Sub
+End Class
